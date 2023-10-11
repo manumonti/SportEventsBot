8d490cdf1ba1757cd24cec6eb6d1da7954bb46be
Manuel Montenegro <manuel@nucypher.com>
2023-10-11 23:58:48 +0200

    Replace Telegram Bot API library
    python-telegram-bot library seems to have a better community support and
    more documentations and examples.

--- bot.py
+++ bot.py
@@ -1,106 +1,144 @@
-import locale
+import logging
 import os
 
-import pymongo
-import telebot
-from telebot.util import quick_markup
-from telebot.callback_data import CallbackData
 from dotenv import load_dotenv
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+)
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 if BOT_TOKEN is None:
     raise AssertionError("BOT_TOKEN variable not found in .env file")
 
-# Show the dates in spanish format
-locale.setlocale(locale.LC_TIME, "es_es")
-
 
 """
-Database
+Sport events creation
 """
-mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+TYPE_STATE, DATE_STATE, PLACE_STATE, MIN_PLAYERS_STATE = range(4)
 
 
-"""
-Bot
-"""
-bot = telebot.TeleBot(BOT_TOKEN)
-bot.delete_my_commands()
-bot.set_my_commands(
-    commands=[telebot.types.BotCommand("nuevo", "Crea un nuevo evento")]
-)
+async def new_sport_event(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    logger.info("User %s started creating a new sport event", user.first_name)
+    keyboard = [
+        [
+            InlineKeyboardButton("Football", callback_data="football"),
+            InlineKeyboardButton("Volleyball", callback_data="volleyball"),
+        ],
+        [InlineKeyboardButton("Otro", callback_data="other")],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Tipo de evento", reply_markup=reply_markup)
+
+    return TYPE_STATE
 
 
-@bot.message_handler(commands=["start"])
-def send_welcome(message):
-    db = mongo_client["sportEventsBot"]
-
-    if message.chat.type == "group":
-        if str(message.chat.id) not in db.list_collection_names():
-            db[str(message.chat.id)].insert_one({"chatName": message.chat.title})
-
-    bot.reply_to(message, "Bot para gestionar eventos")
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    pass
 
 
-"""
-Create a new sport event
-"""
-event_type_cb_data_factory = CallbackData("event_type", prefix="event_type")
+async def event_type(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    # TODO: save the event type
+    query = update.callback_query
+    await query.answer()
+    keyboard = [
+        [
+            InlineKeyboardButton("Hoy", callback_data="today"),
+            InlineKeyboardButton("Mañana", callback_data="tomorrow"),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
-event_type_markup = {
-    "Football": {
-        "callback_data": event_type_cb_data_factory.new(event_type="football")
-    },
-    "Volleyball": {
-        "callback_data": event_type_cb_data_factory.new(event_type="volleyball")
-    },
-    "Otro": {"callback_data": event_type_cb_data_factory.new(event_type="other")},
-    "Cancelar": {"callback_data": "cancel"},
-}
+    await query.edit_message_text(text="Fecha", reply_markup=reply_markup)
 
-date_markup = {
-    "Hoy": {"callback_data": "date_today"},
-    "Mañana": {"callback_data": "date_tomorrow"},
-    "Cancelar": {"callback_data": "cancel"},
-}
+    return DATE_STATE
 
 
-@bot.callback_query_handler(func=lambda call: call.data == "cancel")
-def callback_cancel(call):
-    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+async def event_date(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    # TODO: save the event date
+    query = update.callback_query
+    await query.answer()
+    keyboard = [
+        [
+            InlineKeyboardButton("El Campito", callback_data="campito"),
+            InlineKeyboardButton("El Cañadón", callback_data="cañadon"),
+            InlineKeyboardButton("Playa del Ilunion", callback_data="ilunion"),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await query.edit_message_text(text="Lugar", reply_markup=reply_markup)
+
+    return PLACE_STATE
 
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith("event_type"))
-def callback_event_type(call):
-    cb_data = event_type_cb_data_factory.parse(callback_data=call.data)
-    if cb_data["event_type"] == "football":
-        bot.answer_callback_query(call.id, "Aún no implementado")
-        print("football")
-    elif cb_data["event_type"] == "volleyball":
-        bot.answer_callback_query(call.id, "Aún no implementado")
-        print("volleyball")
-    elif cb_data["event_type"] == "other":
-        bot.answer_callback_query(call.id, "Aún no implementado")
-        print("other")
+async def event_place(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    # TODO: save the event place
+    query = update.callback_query
+    await query.answer()
+    keyboard = [
+        [
+            InlineKeyboardButton("4", callback_data="4"),
+            InlineKeyboardButton("6", callback_data="6"),
+            InlineKeyboardButton("10", callback_data="10"),
+            InlineKeyboardButton("14", callback_data="14"),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
-    bot.edit_message_text(
-        chat_id=call.message.chat.id,
-        message_id=call.message.message_id,
-        text="Fecha",
-        reply_markup=quick_markup(values=date_markup, row_width=2),
+    await query.edit_message_text(text="Mínimo de jugadores", reply_markup=reply_markup)
+
+    return MIN_PLAYERS_STATE
+
+
+async def event_min_players(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    # TODO: save the event min players
+    # TODO: save the new event in DB
+    user = update.callback_query.from_user
+
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(text="Evento registrado!")
+
+    logger.info("User %s created a new event", user.first_name)
+
+    return ConversationHandler.END
+
+
+def main() -> None:
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    new_sport_event_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("nuevo", new_sport_event)],
+        states={
+            TYPE_STATE: [CallbackQueryHandler(event_type)],
+            DATE_STATE: [CallbackQueryHandler(event_date)],
+            PLACE_STATE: [CallbackQueryHandler(event_place)],
+            MIN_PLAYERS_STATE: [CallbackQueryHandler(event_min_players)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
     )
 
+    application.add_handler(new_sport_event_conv_handler)
 
-@bot.message_handler(commands=["nuevo"])
-def new_event_handler(message):
-    bot.send_message(
-        chat_id=message.chat.id,
-        text="*Nuevo evento*",
-        parse_mode="Markdown",
-        reply_markup=quick_markup(values=event_type_markup, row_width=3),
-    )
-    bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    application.run_polling()
 
 
-bot.infinity_polling()
+if __name__ == "__main__":
+    main()
